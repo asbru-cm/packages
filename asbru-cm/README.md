4ba7fe6a6594f9a2932b1066c8cc01d3ce1e54ee
Klaas Tammling <klaas@tammling.hamburg>
2017-09-21 14:23:11 +0200

    Readme for building asbru-cm and getting rid of the root privileges (yeah)

---
+++ asbru-cm/README.md
@@ -0,0 +1,7 @@
+# Building packages
+## Debian
+
+1. Clone this repository somewhere you like
+2. Move into the asbru-cm/ folder
+3. Run ./make_debian.sh
+4. The packages will all be created under ./tmp/ you can copy them from there and delete the tmp folder afterwards.
--- asbru-cm/make_debian.sh
+++ asbru-cm/make_debian.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-if [ "$(whoami)" != "root" ]; then
-	echo "Sorry you have to use sudo to run this script because we need to write to /tmp."
-	echo "We know this isn't a good way to go and we are thinking about a better way to handle this. For the moment please run me with sudo ./make_debian.sh"
-	exit 1
-fi
-
 if [[ -z "$1" ]]; then
 	echo "Please give me a release name matching GitHub. It is case sensitive like 5.0.0-RC1" 1>&2
 	echo "You can find releases under https://github.com/asbru-cm/asbru-cm/releases" 1>&2
 	exit 1
 fi
 
-rm -rf /tmp/asbru-packaging
-mkdir /tmp/asbru-packaging
 
+
+PACKAGE_DIR=./tmp
 RELEASE=$1
 RELEASE_DEBIAN=${1,,}
 
-wget -q https://github.com/asbru-cm/asbru-cm/archive/$RELEASE.tar.gz -O /tmp/asbru-packaging/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz
+rm -rf $PACKAGE_DIR
+mkdir $PACKAGE_DIR
+
+wget -q https://github.com/asbru-cm/asbru-cm/archive/$RELEASE.tar.gz -O $PACKAGE_DIR/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz
 
 if [ $? -ne 0 ]; then
 	echo "An error occured while downloading release $RELEASE"
@@ -26,10 +23,12 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-tar -xzf /tmp/asbru-packaging/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz -C /tmp/asbru-packaging/
+tar -xzf /tmp/asbru-packaging/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz -C $PACKAGE_DIR
 
-cp -R debian/ /tmp/asbru-packaging/asbru-cm-$RELEASE/debian/
+cp -R debian/ $PACKAGE_DIR/asbru-cm-$RELEASE/debian/
 
-cd /tmp/asbru-packaging/asbru-cm-$RELEASE/
+cd $PACKAGE_DIR/asbru-cm-$RELEASE/
 
 debuild -us -uc
+
+echo "If everything went good you will find your packages here under $PACKAGE_DIR"
