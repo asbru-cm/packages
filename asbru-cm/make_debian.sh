b466f0f7b59ed453e2af57cc408df2ccab1fbe12
Gaëtan Frenoy <gaetan@frenoy.net>
2017-09-30 22:21:22 +0200

    Get latest version automatically from github + update change log for rc2

--- asbru-cm/make_debian.sh
+++ asbru-cm/make_debian.sh
@@ -1,24 +1,63 @@
 #!/bin/bash
 
+# Some working variables
+G="\033[32m"
+B="\033[39m"
+Y="\033[33m"
+OK="${G}OK${B}"
+ERROR="${Y}ERROR${B}"
+
+# Let's check some basic requirements
 if ! [ -x "$(command -v debuild)" ]; then
-	echo "debuild is required, did you install the 'devscripts' package yet ?"
-	exit 1
+  echo "debuild is required, did you install the 'devscripts' package yet ?"
+  exit 1
+fi
+
+# Some other optional requirements
+if [ -x "$(command -v jq)" ]; then
+  JQ=$(command -v jq)
+else
+  JQ=""
+fi
+if [ -x "$(command -v curl)" ]; then
+  CURL=$(command -v curl)
+else
+  CURL=""
 fi
 
 if [[ -z "$1" ]]; then
-	# DevNote:
-	#   If not specified, we should get the latest release from
-	#     curl "https://api.github.com/repos/asbru-cm/asbru-cm/tags"
-	#
-	echo "Please give me a release name matching GitHub. It is case sensitive like 5.0.0-RC1" 1>&2
-	echo "You can find releases under https://github.com/asbru-cm/asbru-cm/releases" 1>&2
-	exit 1
+  if [ ! "$JQ" == "" ] && [ ! "$CURL" == "" ] ; then
+    # Try to guess the latest release
+    echo -n "No release given, querying GitHub ..."
+    all_releases=$(${CURL} -s https://api.github.com/repos/asbru-cm/asbru-cm/tags)
+    if [ $? -eq 0 ] ; then
+      echo -e " $OK !"
+      echo -n -e "Extracting latest version ..."
+      RELEASE=$(echo $all_releases | ${JQ} -r '.[0] | .name')
+      echo -e " found [$RELEASE], $OK !"
+    fi
+  fi
+else
+  RELEASE=$1
+fi
+
+if [[ -z "$RELEASE" ]] ; then
+  echo -e "${ERROR}"
+  echo "Please provide a release name matching GitHub. It is case sensitive like 5.0.0-RC1" 1>&2
+  echo "You can find Ásbrú releases at https://github.com/asbru-cm/asbru-cm/releases" 1>&2
+  echo " " 1>&2
+  echo "If you want this tool to guess the latest release automatically, make sure the following tools are available:" 1>&2
+  echo " - curl (https://curl.haxx.se/)" 1>&2
+  echo " - jq (https://stedolan.github.io/jq)" 1>&2
+  echo " " 1>&2
+  exit 1
 fi
 
 PACKAGE_DIR=./tmp
-RELEASE=$1
-RELEASE_DEBIAN=${1,,}
+RELEASE_DEBIAN=${RELEASE,,}
 DEBIAN_VERSION=${RELEASE_DEBIAN/-/"~"}
+
+echo -n "Building package release ${DEBIAN_VERSION}, be patient ..."
 
 rm -rf $PACKAGE_DIR
 mkdir $PACKAGE_DIR
@@ -26,9 +65,9 @@ mkdir $PACKAGE_DIR
 wget -q https://github.com/asbru-cm/asbru-cm/archive/$RELEASE.tar.gz -O $PACKAGE_DIR/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz
 
 if [ $? -ne 0 ]; then
-	echo "An error occured while downloading release $RELEASE"
-	echo "Please check if that release actually exists and the server isn't down"
-	exit 1
+  echo "An error occured while downloading release $RELEASE"
+  echo "Please check if that release actually exists and the server isn't down"
+  exit 1
 fi
 
 tar -xzf $PACKAGE_DIR/asbru-cm_$RELEASE_DEBIAN.orig.tar.gz -C $PACKAGE_DIR
@@ -39,6 +78,14 @@ cd $PACKAGE_DIR/asbru-cm-$RELEASE/
 
 mv ../asbru-cm_$RELEASE_DEBIAN.orig.tar.gz ../asbru-cm_$DEBIAN_VERSION.orig.tar.gz
 
-debuild -us -uc
+debuild -us -uc > /dev/null
 
-echo "If everything went good you will find your packages here under $PACKAGE_DIR"
+if [ $? -eq 0 ] ; then
+  echo -e " $OK !"
+  echo "This look like good news, package succesfully build in $PACKAGE_DIR :)"
+else
+  echo -e "${ERROR}"
+  echo "Bad news, something did not happen as expected, check .build file in $PACKAGE_DIR to get more information."
+fi
+
+echo "All done."
